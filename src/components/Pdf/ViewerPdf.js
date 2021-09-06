8793120303f161a3dda0261c7fa401c44c86fc76
jpampa <jpampa@apros.pe>
2021-09-06 17:58:01 -0500

    pdf

--- src/components/Pdf/ViewerPdf.js
+++ src/components/Pdf/ViewerPdf.js
@@ -20,7 +20,7 @@ export default function ViewerPdf() {
       {/*    url: 'https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf',*/}
       {/*  }}*/}
       {/*/>*/}
-      <PDFObject url='https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf' />
+      <PDFObject url='https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf' width="100%" height="100vh" forcePDFJS={true}/>
       {/*<object*/}
       {/*  data="https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf"*/}
       {/*  type="application/pdf"*/}
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ const IndexPage = () => (
     {/*<Buscador/>*/}
     {/*<ScrollAnimation/>*/}
     {/*<ArchivoPdf/>*/}
-    {/*<ViewerPdf/>*/}
-    <Step/>
+    <ViewerPdf/>
+    {/*<Step/>*/}
   </Layout>
 )
 
