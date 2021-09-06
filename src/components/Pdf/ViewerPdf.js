31fec80c43527e4619c0c5789ac76156241c88b0
jpampa <jpampa@apros.pe>
2021-09-06 18:33:48 -0500

    commit

--- src/components/Pdf/ViewerPdf.js
+++ src/components/Pdf/ViewerPdf.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import DocViewer, { PDFRenderer , PNGRenderer } from "react-doc-viewer";
 // import PDFViewer from 'pdf-viewer-reactjs'
 
-import { PDFObject }  from 'react-pdfobject'
+import { PDFObject },view  from 'react-pdfobject'
 
 export default function ViewerPdf() {
 
@@ -20,7 +20,7 @@ export default function ViewerPdf() {
       {/*    url: 'https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf',*/}
       {/*  }}*/}
       {/*/>*/}
-      <PDFObject url='https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf' width="100%" height="100vh" forcePDFJS={true}/>
+      <PDFObject url='https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf' width="100%" height="100vh" forcePDFJS={true} PDFJS_URL={true}/>
       {/*<object*/}
       {/*  data="https://apros-qa.net.pe/armandoparedes/wp-content/uploads/2021/08/Pasaje-Laureles-Cuadernillo-de-planos.pdf"*/}
       {/*  type="application/pdf"*/}
